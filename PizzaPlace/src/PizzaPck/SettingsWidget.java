b27e777cd7f68d976fc50346a56d041ef7455f76
palandm <pal.myrland@hotmail.com>
2011-11-03 11:11:45 +0100

    hopp

--- PizzaPlace/src/PizzaPck/SettingsWidget.java
+++ PizzaPlace/src/PizzaPck/SettingsWidget.java
@@ -8,6 +8,8 @@ import com.trolltech.qt.gui.QGroupBox;
 import com.trolltech.qt.gui.QLabel;
 import com.trolltech.qt.gui.QLineEdit;
 import com.trolltech.qt.gui.QPushButton;
+import com.trolltech.qt.gui.QSizePolicy.Policy;
+import com.trolltech.qt.gui.QVBoxLayout;
 import com.trolltech.qt.gui.QWidget;
 
 
@@ -21,9 +23,10 @@ public class SettingsWidget extends QWidget {
 	
 	//knapper, tekstfelt og labels
 	private QLineEdit txtName, txtContents, txtPrice, txtBorder, txtDelivery;
-	private QLabel labName,labContents,labPrice, labBorder, labDelivery;
+	private QLabel labName,labContents,labPrice, labBorder, labDelivery, txtLimit;
 	private QGridLayout pizzaLayout;
-	private QGridLayout settingsLayout, borderLayout;
+	private QGridLayout borderLayout;
+	QVBoxLayout box;
 	private QPushButton btnAddpizza, btnChange, btnChangeDelivey;
 	private QGroupBox groupBoxPizza;
 	private QGroupBox groupBoxPrice;
@@ -71,12 +74,24 @@ public class SettingsWidget extends QWidget {
 	
 	public void setDeliveyPrice(){
 		String var = txtDelivery.text();
+		if (Float.valueOf(var) < 0){
+			txtLimit.setText("Nåværende grense er " + DELIVERY_LIMIT+ "Kan ikke være negativ");
+		}
+		else{
 		DELIVERY_PRICE = Float.valueOf(var);
+		txtLimit.setText("Nåværende grense er " + DELIVERY_LIMIT+" og nåværende pris er " +DELIVERY_PRICE);
+		}
 	}
 	
 	public void setLimit(){
 		String var = txtBorder.text();
+		if (Float.valueOf(var) < 0){
+			txtLimit.setText("Kan ikke være negativ " + " og nåværende pris er " +DELIVERY_PRICE);
+		}
+		else{
 		DELIVERY_LIMIT = Float.valueOf(var);
+		txtLimit.setText("Nåværende grense er " + DELIVERY_LIMIT+" og nåværende pris er " +DELIVERY_PRICE);
+		}
 	}
 	
 	
@@ -88,68 +103,65 @@ public class SettingsWidget extends QWidget {
 		
 		
 		//creates new instances of buttons, labels, lineEdit, etc
-		settingsLayout = new QGridLayout();
+		box = new QVBoxLayout();
 		pizzaLayout = new QGridLayout();
 		borderLayout = new QGridLayout();
 		
 		
 		groupBoxPizza = new QGroupBox("Legg til nye pizzaer");
 		groupBoxPizza.setLayout(pizzaLayout);
-		this.setLayout(settingsLayout);
+		this.setLayout(box);
 		
 		groupBoxPrice = new QGroupBox("Endre prisgrense for gratis levering og leveringsgebyr");
 		groupBoxPrice.setLayout(borderLayout);
 		
 		labBorder = new QLabel("Ny grense");
 		txtBorder = new QLineEdit();
+		txtBorder.setFixedWidth(200);
 		labDelivery = new QLabel("Ny Leveringspris");
 		txtDelivery = new QLineEdit();
+		txtDelivery.setFixedWidth(200);
 		btnChange = new QPushButton("Endre grensen");
 		btnChangeDelivey = new QPushButton("Endre leveringspris");
 		
-		btnAddpizza = new QPushButton("Legg til pizza i DB");
-		
-		txtName = new QLineEdit();
-		txtContents = new QLineEdit();
-		txtPrice = new QLineEdit();
 		
 		labName = new QLabel("Navn p� pizza:");
+		txtName = new QLineEdit();
+		txtName.setFixedWidth(200);
 		labContents = new QLabel("Ingredienser");
+		txtContents = new QLineEdit();
+		txtContents.setFixedWidth(200);
 		labPrice = new QLabel("Pris");
+		txtPrice = new QLineEdit();
+		txtPrice.setFixedWidth(200);
+		btnAddpizza = new QPushButton("Legg til pizza i DB");
+		txtLimit = new QLabel("Nåværende grense er " + DELIVERY_LIMIT + " og nåværende pris er " +DELIVERY_PRICE);
 		
 		
-		settingsLayout.addWidget(groupBoxPizza);
-		settingsLayout.addWidget(groupBoxPrice);
+		box.addWidget(groupBoxPizza);
+		box.addWidget(groupBoxPrice);
 		
-		
-		borderLayout.addWidget(labBorder, 0, 0);
-		borderLayout.addWidget(txtBorder, 0, 1);
-		borderLayout.addWidget(btnChange, 0, 2);
+		//add buttons, labels and lineedits to borderlayout
+		borderLayout.addWidget(txtLimit, 0, 0, 1, 0);
+		borderLayout.addWidget(labBorder, 1, 0);
+		borderLayout.addWidget(txtBorder, 1, 1);
+		borderLayout.addWidget(btnChange, 1, 2);
+		txtLimit.setSizePolicy(Policy.Fixed, Policy.Fixed);
 		btnChange.clicked.connect(this, "setLimit()");
 		
-		borderLayout.addWidget(labDelivery, 1, 0);
-		borderLayout.addWidget(txtDelivery, 1, 1);
-		borderLayout.addWidget(btnChangeDelivey, 1, 2);
+		borderLayout.addWidget(labDelivery, 2, 0);
+		borderLayout.addWidget(txtDelivery, 2, 1);
+		borderLayout.addWidget(btnChangeDelivey, 2, 2);		
 		btnChangeDelivey.clicked.connect(this, "setDeliveyPrice()");
-		
-		
 		
 		//add buttons, labels and lineedits to pizzalayout
 		pizzaLayout.addWidget(labName, 0, 0);
-		pizzaLayout.addWidget(txtName, 0, 1);
-		
+		pizzaLayout.addWidget(txtName, 0, 1);		
 		pizzaLayout.addWidget(labContents, 1, 0);
-		pizzaLayout.addWidget(txtContents, 1, 1);
-		
+		pizzaLayout.addWidget(txtContents, 1, 1);		
 		pizzaLayout.addWidget(labPrice, 2, 0);
-		pizzaLayout.addWidget(txtPrice, 2, 1);
-		
+		pizzaLayout.addWidget(txtPrice, 2, 1);		
 		pizzaLayout.addWidget(btnAddpizza, 3, 2);
-		
-		//
 		btnAddpizza.clicked.connect(this, "insertIntoDB()");
 	}
-	
-	
-	
 }
