aab03293d38dbdfdd5f7ff5477a52ec8f3c2f163
palandm <pal.myrland@hotmail.com>
2011-11-10 15:55:42 +0100

    errordocs

--- PizzaPlace/src/PizzaPck/ErrorMessage.java
+++ PizzaPlace/src/PizzaPck/ErrorMessage.java
@@ -13,16 +13,36 @@ import com.trolltech.qt.gui.QWidget;
 
 public class ErrorMessage {
 	
+	/**
+	 * User doesn't exist in database, messagebox is displayed.
+	 * @param parent
+	 */
 	public static void noSuchUser(QWidget parent){
 		QMessageBox.information(parent, "Info", "Brukeren finnes ikke i databasen");
 	}
+	
+	/**
+	 * Invalid input/No input, messagebox is displayed.
+	 * @param parent
+	 */
 	public static void invalidInput(QWidget parent){
 		QMessageBox.information(parent, "Info", "Alle feltene er ikke utfylt");
 	}
+	/**
+	 * messagebox is displayed if database-error occurs.
+	 * @param parent
+	 * @param s
+	 */
 	
 	public static void databaseError(QWidget parent, String s){
 		QMessageBox.warning(parent, "Database error", s);
 	}
+	/**
+	 * messagebox is displayed if the program fails to get 
+	 * products from the database
+	 * @param parent
+	 * @param s
+	 */
 	
 	public static void failMenu(QWidget parent, String s){
 		
